022bbbf0c2e4e50e366a73d9686773e4f85ac6f8
Jaime <jaime.joseph1991@gmail.com>
2022-05-20 13:13:23 -0400

    completed styling, added more js functionality

--- js/main.js
+++ js/main.js
@@ -7,12 +7,15 @@
 
 const button = document.querySelector('button')
 const textPlace = document.querySelector('.advice-text')
+const adviceBox = document.querySelector('.advice-text-box')
 
 button.addEventListener('click',() => {
     
     getFetch().catch(err => `Advice not found: ${err}`)
 })
 
+//Hide advice box on page load
+adviceBox.style.display = 'none'
 
 const getFetch = async () => {
    
@@ -20,6 +23,8 @@ const query = await document.querySelector('.input-box').value
 const response = await fetch(`https://api.adviceslip.com/advice/search/${query}`)
 const data = await response.json()
 //return data
+    adviceBox.style.display = 'flex'
 textPlace.textContent = data.slips[0].advice
+
 console.log(data.slips[0].advice)
 }
